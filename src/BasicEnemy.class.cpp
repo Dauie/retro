693ad3d05805c2f5b7cd6ab7acd3ed893f5ec4fa
Robert LUTT <ausdauerr@gmail.com>
2018-01-14 19:26:35 -0800

    small change to enemybasic

--- src/BasicEnemy.class.cpp
+++ src/BasicEnemy.class.cpp
@@ -31,10 +31,7 @@ BasicEnemy::BasicEnemy(float dirX, float dirY, float posX, float posY)
 
 BasicEnemy::~BasicEnemy(void)
 {
-	std::system("afplay ./sounds/explosion.mp3 &");
-	for (int i = 0; i < 10; i++) {
-		new Particle(_posX, _posY);
-	}
+
 }
 
 void	BasicEnemy::update(void)
@@ -42,6 +39,7 @@ void	BasicEnemy::update(void)
 	if (!_alive)
 	{
 		if (!_wall) {
+			std::system("afplay ./sounds/explosion.mp3 &");
 			for (int i = 0; i < 10; i++) {
 				new Particle(_posX, _posY);
 			}
